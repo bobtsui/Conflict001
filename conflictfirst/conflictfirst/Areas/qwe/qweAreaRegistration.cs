a8f71ba2ae7830c6d349e026d9a4f20bbce1b29c
bobtsui <grimmo58968@gmail.com>
2017-09-27 20:45:42 +0800

    001-註解OOXX123456

---
+++ conflictfirst/conflictfirst/Areas/qwe/qweAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace conflictfirst.Areas.qwe
+{
+    public class qweAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "qwe";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "qwe_default",
+                "qwe/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
